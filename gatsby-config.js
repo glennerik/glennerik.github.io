6bb76c40c89106780c2e22b25624cf2a1f9ad1dd
Glenn-Erik Sandbakken <glennerik@gmail.com>
2019-10-07 16:55:40 +0200

    add emoji-party favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Glenn-Erik Sandbakken`,
-    description: `www.glennerik.no - My Website`,
+    description: `www.glennerik.no - My Place on the Internet`,
     author: `@glenneriks`,
   },
   plugins: [
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Emoji-Party.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
